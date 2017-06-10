1bbc3c3e8a4772b136843e0b272c4de0a7367627
IvanKenevich <roranmolotoboyez@gmail.com>
2017-06-10 12:17:34 -0700

    Re-write most of the SlopePanel class.

--- src/Driver.java
+++ src/Driver.java
@@ -1,21 +1,15 @@
-import java.awt.*;
 import javax.swing.*;
-/**
- * Write a description of class Driver here.
- * 
- * @author (your name) n
- * @version (a version number or a date)
- */
+import java.awt.*;
+
 public class Driver
 {
     public static void main(String[] args)
     {
         JFrame f = new JFrame("Slope Field");
-        SlopePanel p = new SlopePanel();
-        
-        f.add(p);
-        f.setSize(760,760);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        Insets i = f.getInsets();
+        f.add(new SlopePanel(f.getWidth() - i.right - i.left,f.getHeight() - i.top - i.bottom));
         f.setVisible(true);
     }
 }
--- src/SlopePanel.java
+++ src/SlopePanel.java
@@ -1,67 +1,45 @@
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.geom.AffineTransform;
 import javax.swing.*;
-import javax.swing.Timer;
-/**
- * Write a description of class Panel here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class SlopePanel extends JPanel implements ActionListener
-{
-    
-    int vx,vy,dx,dy,expr,offset,increment;
-    int r;
-    Timer t;
-    
-    public SlopePanel()
-    {
-        vx=0;
-        dx=0;
-        vy=0;
-        dy=0;
-        r=5;
-        offset=0;
-        increment=1;
-        t = new Timer(40,this);
-        t.start();
-    }
-    
-    public void drawLines(Graphics g)
-    {
-        g.setColor(Color.black);
-        for (int i = -19; i<19; i++)
-        {
-            for (int j = -19; j<=19; j++)
-            {
-               expr=i*j+offset;
 
-               dx=1;
-               dy=dx*expr;
-               g.drawLine(2*i*10-dx,2*j*10-dy,2*i*10+dx,2*j*10+dy);
+public class SlopePanel extends JPanel {
+    private int width, height,
+            minX, maxX, minY, maxY;
+
+    public SlopePanel(int width, int height) {
+        this.width = width;
+        this.height = height;
+
+        minX = Math.round(-width / 2);
+        maxX = Math.round(width / 2);
+        minY = Math.round(-height / 2);
+        maxY = Math.round(height / 2);
+    }
+
+    public void drawCoordinateAxes(Graphics2D g) {
+        g.drawLine(minX, 0, maxX, 0); // x-axis
+        g.drawLine(0, minY, 0, maxY); //y-axis
+    }
+
+    public void drawGrid(Graphics2D g, int stepX, int stepY) {
+        for (int x = minX; x <= maxX; x += stepX) {
+            for (int y = minY; y <= maxY; y += stepY) {
+                int slope = (x + y)/8;
+                double theta = Math.atan(slope);
+                g.drawLine((int)(x - 5*Math.cos(theta)), (int)(y - 5*Math.sin(theta)),
+                        (int)(x + 5*Math.cos(theta)), (int)(y + 5*Math.sin(theta)));
             }
         }
     }
-    
-    public void actionPerformed (ActionEvent e)
-    {
-        if (offset==20 || offset==-20)
-        {increment=-increment;}
-        offset+=increment;
-        expr+=offset;
-        repaint();
-    }
-    
-    public void paintComponent(Graphics g)
-    {
+
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        
-        g.translate(380,380);
-              
-        
-        g.drawLine(-380,0,380,0);
-        g.drawLine(0,380,0,-380);
-        drawLines(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setTransform(new AffineTransform(1, 0, 0, -1, Math.round(width / 2), Math.round(height / 2)));
+
+        g2.setColor(Color.BLACK);
+        drawCoordinateAxes(g2);
+        drawGrid(g2, Math.round(width/8),Math.round(height/8));
     }
 }
